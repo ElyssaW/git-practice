f7b682a8d8a6965f94c8bbf6915663891cf8a598
tkadima <kadima.t@husky.neu.edu>
2021-02-10 16:15:17 -0500

    me talking to myself about myself

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Jeff from './Jeff'
 import Elyssa from './Elyssa'
 import Woo from './Woo'
 import About from './About'
+import GrinchYoga from './GrinchYoga'
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
       <Elyssa />
       
       <Woo />
+      <GrinchYoga />
     </div>
   )
 }
