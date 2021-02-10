cd502f2b2ed9458881ef6fcf294e016b202f7704
ElyssaW <ejwinch1@gmail.com>
2021-02-10 12:57:42 -0600

    Added About Me for myself (Elyssa)

---
+++ src/Elyssa.js
@@ -0,0 +1,10 @@
+function Elyssa () {
+    return (
+        <div>
+            <h1>Hello, I'm Elyssa!</h1>
+            <p>I like to cook, draw, and play video games</p>
+        </div>
+    )
+}
+
+export default Elyssa
