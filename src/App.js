b31b0663d3ee26cd2248b487f599e16e874c15fe
ChassyAHustla <chassyahustla@ChassyAHustlas-MacBook-Pro.local>
2021-02-10 11:04:34 -0800

    added import

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Chassity from './components/Chassity'
 
 function App() {
   return (
     <div className="App">
+      <Chassity />
     </div>
   );
 }
